fcb1d68e4d345d29d0e87fc853ecb15c95301f1e
Eric Seidel <gridaphobe@gmail.com>
2014-01-30 14:07:16 -0800

    stitch together sbv, refts, and hint to generate test cases for refinements and dynamically check.
    only works for integer functions of single arguments at the moment..

--- LiquidGen.hs
+++ LiquidGen.hs
@@ -1,16 +1,83 @@
+{-# LANGUAGE PackageImports #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module LiquidGen where
 
-import Control.Applicative
-import Control.Monad.Reader
+import           Control.Applicative
+import           Control.Exception.Base
+import "mtl"     Control.Monad.Reader
 import qualified Data.Map as M
-import Data.SBV
-import Language.Fixpoint.Parse
-import Language.Fixpoint.Types hiding (Symbolic)
-import Language.Haskell.Liquid.Parse ()
-import Language.Haskell.Liquid.Types
+import           Data.Typeable
+import           Test.QuickCheck hiding ((==>))
+import           Text.Printf
 
-test s = sat $ runReaderT (genT (rr s :: BareType)) M.empty
+import           Data.SBV
+import           Language.Fixpoint.Parse
+import           Language.Fixpoint.Types hiding (Symbolic)
+import qualified Language.Haskell.Interpreter as Hint
+import           Language.Haskell.Liquid.Parse ()
+import           Language.Haskell.Liquid.Types
+
+
+{-
+
+NOTES
+-----
+
+QuickCheck actually has special cases for some values that are
+difficult to generate, e.g. sorted lists. The approach is to generate
+arbitrary values and then apply the predicate to them:
+
+    sort <$> vector 10 :: Gen [Int]
+
+This is of course orders of magnitude faster than contraining the
+generator to produce pre-sorted lists
+
+    vector 10 `suchThat` isSorted :: Gen [Int]
+
+Are there cases where we can't rely on post-processing to ensure a
+property holds? Sure
+
+    arbitrary `suchThat` \(x,y,z) -> y == f x && z == g y
+
+For some sufficiently complex `f` and `g`.
+
+-}
+
+
+test s = runReaderT (genT (rr s :: BareType)) M.empty
+
+
+test' spec func
+  = do let (cs, r) = genPrePost $ rr spec
+       sats <- take 20 . extractModels <$> allSat (runReaderT cs M.empty)
+       f    <- eval func
+       -- FIXME: this is specializing to function of single arg, must generalize
+       forM_ sats $ \x -> do
+         v <- evaluate (show $ f x) `catch` \(_ :: SomeException) -> return "CRASH"
+         b <- evaluate (evalReft (M.fromList [("x",x)]) r (f x))
+              `catch` \(_ :: SomeException) -> return False
+         let k = if b
+                 then "SAFE"
+                 else "UNSAFE"
+         printf "%s:\tx = %d, f x = %s\n" k x v
+
+
+eval :: Typeable a => String -> IO a
+eval s = do r <- Hint.runInterpreter $ do Hint.setImportsQ [("Prelude", Nothing)]
+                                          Hint.interpret s Hint.infer
+            case r of
+              Left err -> error $ show err
+              Right v  -> return v
+
+genPrePost :: BareType -> (Convert SBool, Reft)
+genPrePost t = (genT t', r)
+  where
+    (xs, ts, rt) = bkArrow t
+    t' = mkArrow [] [] (zip (init xs) (init ts)) (last ts)
+    r  = toReft $ rt_reft rt
+
+
+
 
 genT :: BareType -> Convert SBool
 genT (RApp c as ps r)
@@ -73,6 +140,41 @@ ofBop Times = (*)
 ofBop Div   = sDiv
 ofBop Mod   = sMod
 
+
+evalReft :: M.Map String Integer -> Reft -> Integer -> Bool
+evalReft m (Reft (S v, rs)) x = and [ fromPred p (M.insert v x m)
+                                    | r <- rs, let p = toPred r
+                                    ]
+
+fromPred :: Pred -> M.Map String Integer -> Bool
+fromPred PTrue           m = True
+fromPred PFalse          m = False
+fromPred (PAnd ps)       m = and [fromPred p m | p <- ps]
+fromPred (POr ps)        m = or  [fromPred p m | p <- ps]
+fromPred (PNot p)        m = not $ fromPred p m
+fromPred (PImp p q)      m = undefined
+fromPred (PIff p q)      m = undefined
+fromPred (PBexp e)       m = undefined -- ofExpr e
+fromPred (PAtom b e1 e2) m = fromBrel b (fromExpr e1 m) (fromExpr e2 m)
+fromPred (PAll ss p)     m = undefined
+fromPred PTop            m = undefined
+
+fromBrel Eq = (==)
+fromBrel Ne = (/=)
+fromBrel Gt = (>)
+fromBrel Ge = (>=)
+fromBrel Lt = (<)
+fromBrel Le = (<=)
+
+fromExpr (EVar (S s))   m = m M.! s
+fromExpr (EBin b e1 e2) m = fromBop b (fromExpr e1 m) (fromExpr e2 m)
+fromExpr (ECon (I i))   m = i
+
+fromBop Plus  = (+)
+fromBop Minus = (-)
+fromBop Times = (*)
+fromBop Div   = div
+fromBop Mod   = mod
 
 --------------------------------------------------------------------------------
 --- STUPID Functor -> Applicative -> Monad NONSENSE
