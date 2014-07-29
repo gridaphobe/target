6129c3f0f6ec6f98a7af5bafd78e9ce7c6c3d635
Eric Seidel <gridaphobe@gmail.com>
2014-07-28 21:36:36 -0700

    blah

--- src/Test/LiquidCheck/Util.hs
+++ src/Test/LiquidCheck/Util.hs
@@ -35,28 +35,6 @@ safeFromJust :: String -> Maybe a -> a
 safeFromJust msg Nothing  = error $ "safeFromJust: " ++ msg
 safeFromJust _   (Just x) = x
 
-silently :: IO a -> IO a
-silently act = act
-silently !act = do
-    hFlush stdout
-    hFlush stderr
-    bracket setup reset $ \(act, o, e, n) -> act >>= \x -> return $! x
-  where
-    setup
-      = do o <- hDuplicate stdout
-           e <- hDuplicate stderr
-           n <- openFile "/dev/null" WriteMode
-           hDuplicateTo n stdout
-           hDuplicateTo n stderr
-           return (act,o,e,n)
-    reset (_,o,e,n)
-      = do hDuplicateTo o stdout
-           hDuplicateTo e stderr
-           hClose o
-           hClose e
-           hClose n
-
-
 applyPreds :: SpecType -> SpecType -> [(Symbol,SpecType)]
 applyPreds sp' dc = zip xs (map tx ts)
   where
