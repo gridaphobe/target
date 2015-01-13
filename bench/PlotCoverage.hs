98a473f07dbf00d677bb77569c2eaaada3cd95d8
Eric Seidel <gridaphobe@gmail.com>
2015-01-12 20:10:02 -0800

    blergh

--- bench/PlotCoverage.hs
+++ bench/PlotCoverage.hs
@@ -5,14 +5,14 @@
 module Main where
 
 import Control.Monad
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy as LB
 import qualified Data.Csv as C
 import qualified Data.Vector as V
 import Data.Maybe
 import Data.String
 import qualified Data.Text as T
-import Graphics.Rendering.Chart.Easy hiding (element, (<.>))
-import Graphics.Rendering.Chart.Backend.Diagrams
+-- import Graphics.Rendering.Chart.Easy hiding (element, (<.>))
+-- import Graphics.Rendering.Chart.Backend.Diagrams
 import Prelude hiding (readFile)
 import Text.Printf
 import Text.XML
@@ -20,8 +20,12 @@ import Text.XML.Cursor
 import System.Environment
 import System.FilePath
 
--- main = do
---   [fn] <- getArgs
+main = do
+  [fn] <- getArgs
+  sts  <- getStats fn
+  withFile (printf "bench/%s.csv" fn) WriteMode $ \h ->
+    mapM_ (LB.hPutStr h . encodeByName header . toNamedRecord) sts
+
   
 
 -- depths :: [Int]
@@ -29,29 +33,29 @@ import System.FilePath
 
 getStats :: String -> IO [HpcStats]
 getStats m = forM ["1", "5", "10", "15", "20", "25", "30"] $ \d -> do
-               -- system $ printf "hpc --xml-output --hpcdir=_results _results/%s-%s.tix"
+               system $ printf "hpc --xml-output --hpcdir=_results _results/%s-%s.tix" m d
                readHpc $ fromString $ printf "%s-%s.xml" m (d::String)
 
-mkPlot name stats = toFile (def {_fo_format = EPS}) (name<.>"eps") $ do
-    layout_title .= name
-    layout_title_style . font_size .= 36
-    layout_legend .= Just (legend_label_style . font_size .~ 24 $ def)
-    layout_x_axis . laxis_style . axis_label_style . font_size .= 24
-    layout_y_axis . laxis_style . axis_label_style . font_size .= 24
-    layout_y_axis . laxis_override .= axisGridHide
-    plotLine "expressions" exprs stats
-    plotLine "booleans" booleans stats
-    plotLine "always True" alwaysTrue stats
-    plotLine "always False" alwaysFalse stats
-    plotLine "alternatives" alts stats
-    plotLine "local functions" local stats
+-- mkPlot name stats = toFile (def {_fo_format = EPS}) (name<.>"eps") $ do
+--     layout_title .= name
+--     layout_title_style . font_size .= 36
+--     layout_legend .= Just (legend_label_style . font_size .~ 24 $ def)
+--     layout_x_axis . laxis_style . axis_label_style . font_size .= 24
+--     layout_y_axis . laxis_style . axis_label_style . font_size .= 24
+--     layout_y_axis . laxis_override .= axisGridHide
+--     plotLine "expressions" exprs stats
+--     plotLine "booleans" booleans stats
+--     plotLine "always True" alwaysTrue stats
+--     plotLine "always False" alwaysFalse stats
+--     plotLine "alternatives" alts stats
+--     plotLine "local functions" local stats
     
-    -- V.forM_ stats $ \s -> plot (line (tool r) [ zip depths (map LogValue $ times r) ] )
+--     -- V.forM_ stats $ \s -> plot (line (tool r) [ zip depths (map LogValue $ times r) ] )
 
-plotLine name f stats = when noNaN $ plot $ line name [ points ]
-  where
-    noNaN  = not $ any (isNaN . snd) points
-    points = [ (depth s, p) | s <- stats, let p = fst (f s) / snd (f s)]
+-- plotLine name f stats = when noNaN $ plot $ line name [ points ]
+--   where
+--     noNaN  = not $ any (isNaN . snd) points
+--     points = [ (depth s, p) | s <- stats, let p = fst (f s) / snd (f s)]
 
 data HpcStats = HpcStats 
   { name         :: T.Text
