5c3c72f2a0dc305669481d2be9f4c7fb5f624895
Eric Seidel <gridaphobe@gmail.com>
2015-06-13 14:51:10 -0700

    add a simple cli tool for running target

--- default.nix
+++ default.nix
@@ -2,18 +2,18 @@
 , exceptions, filepath, ghc, ghc-paths, ghc-prim, liquid-fixpoint
 , liquidhaskell, mtl, pretty, process, stdenv, syb, tagged, tasty
 , tasty-hunit, template-haskell, text, text-format, transformers
-, unordered-containers, vector, th-lift
+, unordered-containers, vector, th-lift, hint
 }:
 mkDerivation {
   pname = "target";
-  version = "0.1.1.1";
+  version = "0.0.0.0";
   src = (import <nixpkgs> {}).fetchgitLocal ./.;
   doCheck = true;
   buildDepends = [
     base containers directory exceptions filepath ghc ghc-paths
     liquid-fixpoint liquidhaskell mtl pretty process syb tagged
     template-haskell text text-format transformers unordered-containers
-    vector th-lift
+    vector th-lift hint
   ];
   testDepends = [
     array base containers deepseq ghc ghc-prim liquid-fixpoint
--- src/Test/Target/TH.hs
+++ src/Test/Target/TH.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
@@ -39,8 +40,13 @@ infoType (TH.VarI _ ty _ _) = ty
 
 deconstructType :: Error -> TH.Type -> TH.Q ([TH.Name], TH.Cxt, TH.Type)
 deconstructType err ty0@(TH.ForallT xs ctx ty) = do
-  let plain (TH.PlainTV _) = True
-      plain _ = False
+  let plain (TH.PlainTV  _)          = True
+#if MIN_VERSION_template_haskell(2,8,0)
+      plain (TH.KindedTV _ TH.StarT) = True
+#else
+      plain (TH.KindedTV _ TH.StarK) = True
+#endif
+      plain _                        = False
   unless (all plain xs) $ err "Higher-kinded type variables in type"
   return (map (\(TH.PlainTV x) -> x) xs, ctx, ty)
 deconstructType _ ty = return ([], [], ty)
