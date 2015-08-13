625c2d7b17d333218cab8f8a650b5fd62534eb26
Eric Seidel <gridaphobe@gmail.com>
2015-08-13 10:50:20 -0700

    update .nix files

--- default.nix
+++ default.nix
@@ -1,25 +1,28 @@
 { mkDerivation, array, base, containers, deepseq, directory
-, exceptions, filepath, ghc, ghc-paths, ghc-prim, liquid-fixpoint
-, liquidhaskell, mtl, pretty, process, stdenv, syb, tagged, tasty
-, tasty-hunit, template-haskell, text, text-format, transformers
-, unordered-containers, vector, th-lift, hint
+, exceptions, filepath, ghc, ghc-paths, ghc-prim, hint
+, liquid-fixpoint, liquidhaskell, mtl, pretty, process, QuickCheck
+, stdenv, syb, tagged, tasty, tasty-hunit, template-haskell, text
+, text-format, th-lift, transformers, unordered-containers, vector
+, z3
 }:
 mkDerivation {
   pname = "target";
-  version = "0.0.0.0";
+  version = "9.9.9.9";
   src = (import <nixpkgs> {}).fetchgitLocal ./.;
-  doCheck = true;
+  isLibrary = true;
+  isExecutable = true;
   buildDepends = [
-    base containers directory exceptions filepath ghc ghc-paths
-    liquid-fixpoint liquidhaskell mtl pretty process syb tagged
-    template-haskell text text-format transformers unordered-containers
-    vector th-lift hint
+    base containers directory exceptions filepath ghc ghc-paths hint
+    liquid-fixpoint liquidhaskell mtl pretty process QuickCheck syb
+    tagged template-haskell text text-format th-lift transformers
+    unordered-containers vector
   ];
   testDepends = [
     array base containers deepseq ghc ghc-prim liquid-fixpoint
     liquidhaskell mtl tagged tasty tasty-hunit template-haskell
     unordered-containers
   ];
+  buildTools = [ z3 ];
   description = "Generate test-suites from refinement types";
   license = stdenv.lib.licenses.mit;
 }
--- shell.nix
+++ shell.nix
@@ -1,8 +1,40 @@
-with (import <nixpkgs> {}).pkgs;
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
 
-let hp = haskell.packages.ghc784.override {
-           overrides = config.haskellPackageOverrides or (self: super: {});
-         };
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, array, base, containers, deepseq, directory
+      , exceptions, filepath, ghc, ghc-paths, ghc-prim, hint
+      , liquid-fixpoint, liquidhaskell, mtl, pretty, process, QuickCheck
+      , stdenv, syb, tagged, tasty, tasty-hunit, template-haskell, text
+      , text-format, th-lift, transformers, unordered-containers, vector
+      , z3
+      }:
+      mkDerivation {
+        pname = "target";
+        version = "9.9.9.9";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        buildDepends = [
+          base containers directory exceptions filepath ghc ghc-paths hint
+          liquid-fixpoint liquidhaskell mtl pretty process QuickCheck syb
+          tagged template-haskell text text-format th-lift transformers
+          unordered-containers vector
+        ];
+        testDepends = [
+          array base containers deepseq ghc ghc-prim liquid-fixpoint
+          liquidhaskell mtl tagged tasty tasty-hunit template-haskell
+          unordered-containers
+        ];
+        buildTools = [ z3 ];
+        description = "Generate test-suites from refinement types";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+
 in
 
-(hp.callPackage ./. {}).env
+  if pkgs.lib.inNixShell then drv.env else drv
